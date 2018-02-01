ca81d9129d8aecc21b3e0ba1145655bc487e30e7
Callum Oakley <c.oakley108@gmail.com>
2018-02-01 23:00:40 +0000

    basic functionality working, sans any notion of completion, timings, accuracy etc

--- src/main.js
+++ src/main.js
@@ -1,72 +1,125 @@
 import { app } from 'hyperapp'
 import { h } from 'ijk'
+import {
+  append,
+  concat,
+  length,
+  map,
+  merge,
+  min,
+  prop,
+  reduce,
+  takeWhile,
+  update
+} from 'ramda'
 
-/* Just for testing */
-const rawText =
-  'The quick brown fox jumps over the lazy dog.\n' +
-  'This is another line!\n' +
-  '\n' +
-  'The line above is empty :o\n' +
-  'The quick brown fox jumps over the lazy dog.'
+// TODO some kind of visual bell when input is disallowed (letter at end of
+// line, backspace at beginning of text, etc)
 
-const text = [[]]
-let i = 0
-for (const c of rawText) {
-  if (c === '\n') {
-    i++
-    text.push([])
-  } else {
-    text[i].push(c)
-  }
-}
-/* * * * */
+// TODO construct initial state from a string.
 
-const newLine = ['span', {}, '\n']
+// TODO get appropriate texts from somewhere
+
+// TODO track accuracy and time, display on completion
+
+const newLineChar = ['span', {}, '\n']
 
 const state = {
-  text,
-  input: [[]]
+  text: [
+    [
+      { target: 'H', line: 0, char: 0 },
+      { target: 'e', line: 0, char: 1 },
+      { target: 'l', line: 0, char: 2 },
+      { target: 'l', line: 0, char: 3 },
+      { target: 'o', line: 0, char: 4 },
+      { target: ' ', line: 0, char: 5 }
+    ],
+    [
+      { target: 'w', line: 1, char: 0 },
+      { target: 'o', line: 1, char: 1 },
+      { target: 'r', line: 1, char: 2 },
+      { target: 'l', line: 1, char: 3 },
+      { target: 'd', line: 1, char: 4 },
+      { target: '.', line: 1, char: 5 },
+      { target: ' ', line: 1, char: 6 }
+    ]
+  ],
+  cursor: { line: 0, char: 0 }
+}
+
+const onLetter = key => ({ text, cursor: { line, char } }) => {
+  // A job for lenses? http://ramdajs.com/docs/#lens
+  if (char >= length(text[line]) - 1) {
+    return { text, cursor: { line, char } }
+  }
+  return {
+    text: update(
+      line,
+      update(char, merge(text[line][char], { input: key }), text[line]),
+      text
+    ),
+    cursor: { line, char: char + 1 }
+  }
+}
+
+const onEnter = ({ text, cursor: { line, char } }) => {
+  if (line >= length(text) - 1) {
+    return { text, cursor: { line, char } }
+  }
+  return {
+    text: update(
+      line,
+      update(char, merge(text[line][char], { input: ' ' }), text[line]),
+      text
+    ),
+    cursor: { line: line + 1, char: 0 }
+  }
+}
+
+const onBackspace = ({ text, cursor: { line, char } }) => {
+  if (char <= 0 && line > 0) {
+    line--
+    char = min(length(takeWhile(prop('input'), text[line])), length(text[line]) - 1)
+  } else if (char > 0) {
+    char--
+  }
+  return {
+    text: update(
+      line,
+      update(char, merge(text[line][char], { input: undefined }), text[line]),
+      text
+    ),
+    cursor: { line, char }
+  }
 }
 
 const actions = {
-  keydown: ({ key }) => ({ input }) => {
-    const line = input.length - 1
-    const char = input[line].length - 1
-    console.log(key)
-    if (key === 'Enter') {
-      return { input: [...input, []] }
-    } else if (key === 'Backspace' && line > 0 && char <= 0) {
-      return { input: input.slice(0, line) }
+  keydown: ({ key }) => {
+    console.log(`key: '${key}'`)
+    if (length(key) === 1) {
+      return onLetter(key)
+    } else if (key === 'Enter') {
+      return onEnter
     } else if (key === 'Backspace') {
-      return { input: [...input.slice(0, line), input[line].slice(0, char)] }
-    } else if (key.length === 1) {
-      return { input: [...input.slice(0, line), [...input[line], key]] }
+      return onBackspace
     }
   }
 }
 
-const Char = ({ text, input }, c, line, char) => {
-  const cursorLine = input.length - 1
-  const cursorChar = input[cursorLine].length
-  if (line > cursorLine || (line === cursorLine && char > cursorChar)) {
-    return ['span', {}, c]
-  } else if (cursorLine === line && cursorChar === char) {
-    return ['span', { class: 'cursor' }, c]
-  } else if (text.length <= line || text[line].length <= char) {
-    return ['span', { class: 'error' }, c]
-  } else if (text[line][char] !== input[line][char]) {
-    return ['span', { class: 'error' }, c]
-  } else {
-    return ['span', { class: 'correct' }, c]
-  }
-}
+const Char = cursor => ({ target, input, line, char }) =>
+  cursor.line === line && cursor.char === char
+    ? ['span', { class: 'cursor' }, target]
+    : input
+      ? input === target
+        ? ['span', { class: 'correct' }, input]
+        : ['span', { class: 'error' }, input === ' ' ? '_' : input]
+      : ['span', {}, target]
 
-const Chars = state => state.text
-  .reduce((acc, line, i) => [
-    ...acc,
-    ...line.map((c, j) => Char(state, c, i, j)),
-    newLine
-  ], [])
+const Text = ({ text, cursor }) => reduce(
+  (acc, ln) => append(newLineChar, concat(acc, map(Char(cursor), ln))),
+  [],
+  text
+)
 
 const view = (state, actions) => h('name', 'props', 'children')([
   'div',
@@ -74,7 +127,7 @@ const view = (state, actions) => h('name', 'props', 'children')([
     class: 'text',
     oncreate: () => window.addEventListener('keydown', actions.keydown)
   },
-  Chars(state)]
-)
+  Text(state)
+])
 
 window.main = app(state, actions, view, document.body)
