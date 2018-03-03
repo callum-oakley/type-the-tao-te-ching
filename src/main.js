5e60596135b08fb4e64f3e63789ce0e0fd9b017f
Callum Oakley <c.oakley108@gmail.com>
2018-03-03 18:00:50 +0000

    construct text object from string, suppress non-modified browser shortcuts

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { app } from 'hyperapp'
 import { h } from 'ijk'
 import {
+  adjust,
   append,
+  compose,
   concat,
   length,
   map,
@@ -9,6 +11,7 @@ import {
   min,
   prop,
   reduce,
+  splitEvery,
   takeWhile,
   update
 } from 'ramda'
@@ -16,38 +19,58 @@ import {
 // TODO some kind of visual bell when input is disallowed (letter at end of
 // line, backspace at beginning of text, etc)
 
-// TODO construct initial state from a string.
-
 // TODO get appropriate texts from somewhere
 
 // TODO track accuracy and time, display on completion
 
+// TODO focus a hidden textarea so that we can type `'` etc without triggering
+// shortcuts in firefox
+
 const newLineChar = ['span', {}, '\n']
 
+const text = compose(
+  prop('text'),
+  reduce(
+    ({ text, line, char }, x) => x === '\n'
+      ? {
+        text: append(
+          [],
+          adjust(
+            append({ target: ' ', line, char }),
+            length(text) - 1,
+            text
+          )
+        ),
+        line: line + 1,
+        char: 0
+      }
+      : {
+        text: adjust(
+          append({ target: x, line, char }),
+          length(text) - 1,
+          text
+        ),
+        line,
+        char: char + 1
+      },
+    { text: [[]], line: 0, char: 0 }
+  ),
+  splitEvery(1)
+)
+
 const state = {
-  text: [
-    [
-      { target: 'H', line: 0, char: 0 },
-      { target: 'e', line: 0, char: 1 },
-      { target: 'l', line: 0, char: 2 },
-      { target: 'l', line: 0, char: 3 },
-      { target: 'o', line: 0, char: 4 },
-      { target: ' ', line: 0, char: 5 }
-    ],
-    [
-      { target: 'w', line: 1, char: 0 },
-      { target: 'o', line: 1, char: 1 },
-      { target: 'r', line: 1, char: 2 },
-      { target: 'l', line: 1, char: 3 },
-      { target: 'd', line: 1, char: 4 },
-      { target: '.', line: 1, char: 5 },
-      { target: ' ', line: 1, char: 6 }
-    ]
-  ],
+  text: text(`Returns a curried equivalent of the provided function. The curried function has two unusual capabilities. First, its arguments needn't be provided one at a time. If f is a ternary function and g is R.curry(f), the following are equivalent:
+
+    g(1)(2)(3)
+    g(1)(2, 3)
+    g(1, 2)(3)
+    g(1, 2, 3)
+
+Secondly, the special placeholder value R.__ may be used to specify "gaps", allowing partial application of any combination of arguments, regardless of their positions. If g is as above and _ is R.__, the following are equivalent:`),
   cursor: { line: 0, char: 0 }
 }
 
-const onLetter = key => ({ text, cursor: { line, char } }) => {
+const onChar = key => ({ text, cursor: { line, char } }) => {
   // A job for lenses? http://ramdajs.com/docs/#lens
   if (char >= length(text[line]) - 1) {
     return { text, cursor: { line, char } }
@@ -79,28 +102,38 @@ const onEnter = ({ text, cursor: { line, char } }) => {
 const onBackspace = ({ text, cursor: { line, char } }) => {
   if (char <= 0 && line > 0) {
     line--
-    char = min(length(takeWhile(prop('input'), text[line])), length(text[line]) - 1)
+    char = min(
+      length(takeWhile(prop('input'), text[line])),
+      length(text[line]) - 1
+    )
   } else if (char > 0) {
     char--
   }
   return {
     text: update(
       line,
-      update(char, merge(text[line][char], { input: undefined }), text[line]),
+      update(
+        char,
+        merge(text[line][char], { input: undefined }),
+        text[line]
+      ),
       text
     ),
     cursor: { line, char }
   }
 }
 
+const isModified = event => event.altKey || event.ctrlKey || event.metaKey
+
 const actions = {
-  keydown: ({ key }) => {
-    console.log(`key: '${key}'`)
-    if (length(key) === 1) {
-      return onLetter(key)
-    } else if (key === 'Enter') {
+  keydown: event => {
+    console.log('event:', event)
+    if (length(event.key) === 1 && !isModified(event)) {
+      event.preventDefault()
+      return onChar(event.key)
+    } else if (event.key === 'Enter') {
       return onEnter
-    } else if (key === 'Backspace') {
+    } else if (event.key === 'Backspace') {
       return onBackspace
     }
   }
